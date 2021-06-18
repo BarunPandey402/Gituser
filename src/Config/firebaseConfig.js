da74d640479445aa03244aeebea4070f42eb2464
barun pandey <programmerbarun@gmail.com>
2021-06-18 21:23:09 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAh71k_JPZEHDeMnfUXPK3a1zw3An_fJxc",
+  authDomain: "mygitapp-1347e.firebaseapp.com",
+  databaseURL: "https://mygitapp-1347e.firebaseio.com",
+  projectId: "mygitapp-1347e",
+  storageBucket: "mygitapp-1347e.appspot.com",
+  messagingSenderId: "240647010468",
+  appId: "1:240647010468:web:48e0ce23e89e56d19f8e31",
+  measurementId: "G-KKT9WW79BF",
+};
